2258755dc9b10ecd37281184ae47c53bf0d1b78e
Prashant Sahib <prashantsd97@gmail.com>
2023-06-20 18:59:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import Footer from './components/Footer/Footer'
+import PageNotFound from './components/PageNotFound/PageNotFound'
+
+function App() {
+
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movie/:imdbID' element={<MovieDetail />} />
+          <Route element={<PageNotFound />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  )
+}
+
+export default App
